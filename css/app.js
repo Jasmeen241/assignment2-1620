4cd1b71af800da3a2374bf73aaef4f6b626c5bd4
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-04-05 01:24:45 +0530

    adding styles to buttons

---
+++ css/app.js
@@ -0,0 +1,12 @@
+//planning functions
+//Function1  Save notes
+//when clicked on the the save button the function stores the note in an array
+
+//Function2  to cancel note
+//when clicked on the cancel button
+//the function should clear up the text area
+
+//Function to open side menu
+//when a user clicks on the button
+//opens the side menu and shows the list of notes already created
+
