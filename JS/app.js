116a4aed4df3734a23240642fbbe113c5d11f8c6
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-04-12 01:38:33 +0530

    adding functionality to sidebar menu

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,7 @@
 //Function1  Save notes
 //when clicked on the the save button the function stores the note in an array
 
+
 //Function2  to cancel note
 //when clicked on the cancel button
 //the function should clear up the text area
@@ -9,4 +10,14 @@
 //Function to open side menu
 //when a user clicks on the button
 //opens the side menu and shows the list of notes already created
+var notesarr = []
+function opensidebar(){
+    document.getElementById("sidebar").style.width = "300px";
+    
+}
+
+//function to close sidebar
+function closesidebar(){
+    document.getElementById("sidebar").style.width = "0px";
+}
 
