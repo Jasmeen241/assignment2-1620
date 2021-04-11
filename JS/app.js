0c3fb8718d9ea7a320e8846099e591771c30a6bb
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-04-12 01:39:56 +0530

    adding dark theme

--- JS/app.js
+++ JS/app.js
@@ -21,3 +21,39 @@ function closesidebar(){
     document.getElementById("sidebar").style.width = "0px";
 }
 
+// function to turn dark mode on when clicked
+const checkbox = document.getElementById("checkbox")
+
+
+checkbox.addEventListener("change",function darkmode(){
+    const main= document.getElementById("main")
+    main.classList.toggle("dark");
+
+    const side= document.getElementById("sidebar")
+    side.classList.toggle("dark");
+
+    document.body.classList.toggle("dark")
+
+    const nav = document.getElementById("navbar")
+    nav.classList.toggle("dark");
+
+    const footer = document.getElementById("footer")
+    footer.classList.toggle("dark");
+
+    const btn1 = document.getElementById("open")
+    btn1.classList.toggle("dark");
+
+    const btn2 = document.getElementById("close")
+    btn2.classList.toggle("dark");
+
+    const btn3 = document.getElementById("addnotes")
+    btn3.classList.toggle("dark");
+
+    const txtbtn1 = document.querySelector(".txtbtn1" )
+    txtbtn1.classList.toggle("dark");
+
+    const txtbtn2 = document.querySelector(".txtbtn2" )
+    txtbtn2.classList.toggle("dark");
+  }
+);
+ 
